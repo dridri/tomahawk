4ff5f809ebf76d6b9d3f1275dbcba02158724a7b
dridri <dridri85@gmail.com>
2014-09-29 23:22:39 +0200

    first draft to get rid of phonon and directly use LibVLC

--- src/libtomahawk/audio/AudioEngine_p.h
+++ src/libtomahawk/audio/AudioEngine_p.h
@@ -1,4 +1,4 @@
-
+/*
 #include <phonon/MediaObject>
 #include <phonon/AudioOutput>
 #include <phonon/AudioDataOutput>
@@ -6,6 +6,8 @@
 #include <phonon/Path>
 #include <phonon/Effect>
 #include <phonon/EffectParameter>
+*/
+#include "AudioOutput.h"
 
 #include <stdint.h>
 
@@ -30,8 +32,8 @@ public:
 
 
 public slots:
-    void onStateChanged( Phonon::State newState, Phonon::State oldState );
-    void onAudioDataArrived( QMap< Phonon::AudioDataOutput::Channel, QVector< qint16 > > data );
+    void onStateChanged( AudioOutput::AudioState newState, AudioOutput::AudioState oldState );
+//    void onAudioDataArrived( QMap< Phonon::AudioDataOutput::Channel, QVector< qint16 > > data );
 
 private:
     QSharedPointer<QIODevice> input;
@@ -42,6 +44,8 @@ private:
     Tomahawk::playlistinterface_ptr currentTrackPlaylist;
     Tomahawk::playlistinterface_ptr queue;
 
+    AudioOutput* audioOutput;
+/*
     Phonon::MediaObject* mediaObject;
     Phonon::AudioOutput* audioOutput;
 
@@ -50,7 +54,7 @@ private:
 
     Phonon::AudioDataOutput* audioDataOutput;
     Phonon::Path audioDataPath;
-
+*/
 
     unsigned int timeElapsed;
     bool expectStop;
